2be914fa6d60fba99360848dec7b3cf0a5617912
Kacper <kacperpap@gmail.com>
2024-05-10 18:32:31 +0200

    Adding temperature_converter project directory

---
+++ temperature_converter/fahrenheit_to_celsius_app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import random
+import uuid
+
+app = Flask(__name__)
+
+app_identifier = uuid.uuid4()
+
+@app.route('/api/convert-fahrenheit-to-celsius', methods=['GET'])
+def convert_fahrenheit_to_celsius():
+    fahrenheit_temp_str = request.args.get('fahrenheit')
+    
+    if fahrenheit_temp_str is None or fahrenheit_temp_str.strip() == '':
+        return jsonify({'error': 'Please provide the Fahrenheit temperature as a query parameter.'}), 400
+    
+    try:
+        fahrenheit_temp = float(fahrenheit_temp_str)
+        
+        celsius_temp = (fahrenheit_temp - 32) * 5/9
+
+        return jsonify({'celsius': celsius_temp, 'app_identifier': app_identifier}), 200
+    except ValueError:
+        return jsonify({'error': 'Invalid input. Please provide a valid Fahrenheit temperature.', 'app_identifier': app_identifier}), 400
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'error': 'API not found.', 'app_identifier': app_identifier}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
